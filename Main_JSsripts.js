ff533f958460ee77ee09d73ad816f77761797c08
Colby <ironflame24@yahoo.com>
2017-10-16 15:02:35 -0600

    Update

---
+++ Main_JSsripts.js
@@ -0,0 +1,14 @@
+var buttonCount = 0;
+function selection(a, b) {
+    buttonCount = buttonCount + 1;
+    
+    if (buttonCount == 2) {
+        dataSearch(a, b)
+    } else {
+        
+    }
+        
+}
+function dataSearch(l, m) {
+    
+}
