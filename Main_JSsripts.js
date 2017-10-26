916ecf4a634eaead2141cc9bf579d1ca42453158
Colby <ironflame24@yahoo.com>
2017-10-25 23:45:18 -0600

    df

--- Main_JSsripts.js
+++ Main_JSsripts.js
@@ -1,5 +1,6 @@
 var choiceOne = "null";
 var choiceTwo = "null";
+var pbCups = 
 
 function selection(b) {
     if (document.getElementById(b).style.backgroundColor == "blue") {
@@ -15,14 +16,23 @@ function selection(b) {
         dataSearch(choiceOne, choiceTwo);
         document.getElementById(choiceOne).style.background = "orange";
         document.getElementById(choiceTwo).style.background = "orange";
+        popup();
     }
         
+}
+function popup() {
+    var popup = document.getElementById("popup");
+    popup.classList.toggle("show");
 }
 function dataSearch(arg1, arg2) {
     var fullList = recipies[arg1][arg2];
     return fullList;
 }
 var recipies = {
-    "chocolate": {"iceCream": [{}, {}, {}], "peanutButter": [{}, {}, {}]}
-        
+    "chocolate": {"iceCream": [{}, {}, {}], 
+                  "peanutButter": [{}, {}, {}], 
+                  "cheese": [{}, {}, {}], 
+                  "fruit": [{}, {}, {}], 
+                  "candy": [{}, {}, {}]}
+    "peanutButter": {"iceCream": [{}, {}, {}], "chocolate": [{}, {}, {}], "cheese": [{}, {}, {}], "fruit": [{}, {}, {}], "candy": [{}, {}, {}]}
         }
