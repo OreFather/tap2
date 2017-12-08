2d3b470ef6a4ddbe0ce7f14e6a5828951bb96d66
Colby <ironflame24@yahoo.com>
2017-12-07 21:35:05 -0700

    1/3 chocolate work

--- Main_JSsripts.js
+++ Main_JSsripts.js
@@ -2,8 +2,8 @@ var choiceOne = "null";
 var choiceTwo = "null";
 
 $(document).ready(function(){
-    $(".popuptext").hide();
-    $(".popuptext").css("display:", "none");
+    $(".choice0").hide();
+    $(".choice0").css("display:", "none");
 })
 
 function selection(b) {
@@ -28,6 +28,7 @@ function selection(b) {
 }
 
 
+
 function dataSearch(arg1, arg2) {
     if (arg1 == "coco" || arg2 == "coco") {
     $.getJSON("json/chocolate.json", function(json){
@@ -36,19 +37,27 @@ function dataSearch(arg1, arg2) {
             $("#choice0Image").append("<img src=" + json.ChocolateFruit.imgPath + " >");
             $("#choice0Skill").append(json.ChocolateFruit.skill);
         } else if (arg1 == "cheese" || arg2 == "cheese") {
-            
+            $("#choice0Name").append(json.ChocolateCheese.name);
+            $("#choice0Image").append("<img src=" + json.ChocolateCheese.imgPath + " >");
+            $("#choice0Skill").append(json.ChocolateCheese.skill);
         } else if (arg1 == "candy" || arg2 == "candy") {
-            
+            $("#choice0Name").append(json.ChocolateCandy.name);
+            $("#choice0Image").append("<img src=" + json.ChocolateCandy.imgPath + " >");
+            $("#choice0Skill").append(json.ChocolateCandy.skill);
         } else if (arg1 == "pB" || arg2 == "pB") {
-            
+            $("#choice0Name").append(json.ChocolatePB.name);
+            $("#choice0Image").append("<img src=" + json.ChocolatePB.imgPath + " >");
+            $("#choice0Skill").append(json.ChocolatePB.skill);
         } else if (arg1 == "iceCream" || arg2 == "iceCream") {
-            
+            $("#choice0Name").append(json.ChocolateIceCream.name);
+            $("#choice0Image").append("<img src=" + json.ChocolateIceCream.imgPath + " >");
+            $("#choice0Skill").append(json.ChocolateIceCream.skill);
         }else {
             console.log("Somthing went wrong in dataSearch() - arg1= " + arg1 + " arg2= " + arg2);
         }
         console.log("--Exited chococlate function--");
         $(".flex").hide();
-        $(".popuptext").show();
+        $(".choice0").css("display:", "inline");
         
         
     })
